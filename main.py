0f331e32757eeff0ed49757114ed53d8fa788b93
hodlmod.eth <zasara1980@gmail.com>
2024-06-13 10:17:31 +0500

    fix

--- main.py
+++ main.py
@@ -12,20 +12,35 @@ def read_wallets(file_path):
     with open(file_path, 'r') as f:
         return {line.strip().lower() for line in f}
 
+def parse_allocation(value):
+    try:
+        value = value.replace(',', '').replace('%', '').strip()
+        return float(value)
+    except ValueError:
+        # print(f"Cannot convert value: {value}")
+        return None
+
+def get_allocation_value(row):
+    numeric_values = [parse_allocation(value) for value in row if parse_allocation(value) is not None]
+    for value in numeric_values:
+        if value < 1:
+            return value
+    return numeric_values[0] if numeric_values else 0.0
+
 def process_csv_file(csv_path, wallet_addresses):
     wallet_allocations = {}
     total_allocation = 0.0
 
     with open(csv_path, 'r') as csvfile:
         reader = csv.reader(csvfile)
+        headers = next(reader)
         for row in reader:
             if not any(row):
                 continue
             try:
                 address = row[0].strip().lower()
-                allocation = row[1].strip()
                 if address in wallet_addresses:
-                    allocation_value = float(allocation.rstrip('%'))
+                    allocation_value = get_allocation_value(row[1:])
                     if address in wallet_allocations:
                         wallet_allocations[address] += allocation_value
                     else:
@@ -74,7 +89,7 @@ def main():
     save_to_json(result_dict, output_json_1)
     save_to_json(total_allocation_dict, output_json_2)
     save_to_json(amount_wallets, output_json_3)
-    print(f"Results saved to {output_json_1} and {output_json_2} and {output_json_3}")
+    print(f"Results saved to {output_json_1}, {output_json_2}, and {output_json_3}")
 
 if __name__ == "__main__":
     main()
