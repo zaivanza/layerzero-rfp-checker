e41dc416d47bb97a115e20d9b2148a9dd3daf9b0
hodlmod.eth <zasara1980@gmail.com>
2024-06-13 09:41:14 +0500

    add info + fix

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ wallets_file = 'wallets.txt'
 root_dir = 'RFP Allocations'
 output_json_1 = 'result_1.json'
 output_json_2 = 'result_2.json'
+output_json_3 = 'result_3.json'
 
 def read_wallets(file_path):
     with open(file_path, 'r') as f:
@@ -25,12 +26,16 @@ def process_csv_file(csv_path, wallet_addresses):
                 allocation = row[1].strip()
                 if address in wallet_addresses:
                     allocation_value = float(allocation.rstrip('%'))
-                    wallet_allocations[address] = allocation
+                    if address in wallet_allocations:
+                        wallet_allocations[address] += allocation_value
+                    else:
+                        wallet_allocations[address] = allocation_value
                     total_allocation += allocation_value
             except (IndexError, ValueError) as e:
                 print(f"Error processing row: {row}, error: {e}")
                 continue
 
+    wallet_allocations = {addr: f"{alloc:.12f}%" for addr, alloc in wallet_allocations.items()}
     return wallet_allocations, total_allocation
 
 def process_all_files(root_dir, wallet_addresses):
@@ -56,12 +61,20 @@ def save_to_json(data, file_path):
     with open(file_path, 'w') as f:
         json.dump(data, f, indent=4)
 
+def get_amount_your_wallets_in_protocols(data):
+    results = {}
+    for protocol in data:
+        results[protocol] = len(data[protocol])
+    return results
+
 def main():
     wallet_addresses = read_wallets(wallets_file)
     result_dict, total_allocation_dict = process_all_files(root_dir, wallet_addresses)
+    amount_wallets = get_amount_your_wallets_in_protocols(result_dict)
     save_to_json(result_dict, output_json_1)
     save_to_json(total_allocation_dict, output_json_2)
-    print(f"Results saved to {output_json_1} and {output_json_2}")
+    save_to_json(amount_wallets, output_json_3)
+    print(f"Results saved to {output_json_1} and {output_json_2} and {output_json_3}")
 
 if __name__ == "__main__":
     main()
